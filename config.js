8f1ae9cd100b64e4defa4ad11e1e3e8f3893b16b
ZombieLN <dilishagim@gmail.com>
2025-11-20 18:32:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID: 'VISPER-MD&7ec0da048dc7c757',
+SESSION_ID: 'VISPER-MD&uVUSmKRJ#nPVi5nX9CrEDj9gGM45cR2jq_R5AGa_HalZudr0tnVA',
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
 ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK, 
